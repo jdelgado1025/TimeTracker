07c7bbd7354d7e6005f9a054051a10023ea7fb25
Jose Delgado <jdelgado1025@gmail.com>
2025-02-05 22:26:43 -0800

    Defined and implemented base Service

--- TimeTracker.API/Services/ITimeEntryService.cs
+++ TimeTracker.API/Services/ITimeEntryService.cs
@@ -1,5 +1,10 @@
-﻿namespace TimeTracker.API.Services;
+﻿using TimeTracker.Domain.Entities;
+using TimeTracker.Domain.Models.TimeEntry;
+
+namespace TimeTracker.API.Services;
 
 public interface ITimeEntryService
 {
+    List<TimeEntryResponse> GetAllTimeEntries();
+    List<TimeEntryResponse> CreateTimeEntry(TimeEntryCreateRequest timeEntryRequest);
 }
--- TimeTracker.API/Services/TimeEntryService.cs
+++ TimeTracker.API/Services/TimeEntryService.cs
@@ -1,5 +1,16 @@
-﻿namespace TimeTracker.API.Services;
+﻿using TimeTracker.Domain.Models.TimeEntry;
 
-public class TimeEntryService
+namespace TimeTracker.API.Services;
+
+public class TimeEntryService : ITimeEntryService
 {
+    public List<TimeEntryResponse> CreateTimeEntry(TimeEntryCreateRequest timeEntryRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<TimeEntryResponse> GetAllTimeEntries()
+    {
+        throw new NotImplementedException();
+    }
 }
