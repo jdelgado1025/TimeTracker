731b0980694de9e18dbdb4bba2bfa51b04061e1f
Jose Delgado <jdelgado1025@gmail.com>
2025-02-05 21:50:28 -0800

    Added bare bones DTOs for Time Entry Create Request and Response

---
+++ TimeTracker.Domain/Models/TimeEntry/TimeEntryCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Domain.Models.TimeEntry;
+
+/* Data Transfer Object for Time Entry Request */
+public class TimeEntryCreateRequest
+{
+    
+}
