f487ab557820229d25db1c948996bd41b1d61c24
Jose Delgado <jdelgado1025@gmail.com>
2025-02-12 21:04:57 -0800

    Added Bare bones implementation of Service

--- TimeTracker.API/Services/TimeEntryService.cs
+++ TimeTracker.API/Services/TimeEntryService.cs
@@ -1,9 +1,17 @@
-﻿using TimeTracker.Domain.Models.TimeEntry;
+﻿using TimeTracker.API.Repositories.Time_Entry;
+using TimeTracker.Domain.Models.TimeEntry;
 
 namespace TimeTracker.API.Services;
 
 public class TimeEntryService : ITimeEntryService
 {
+    private readonly ITimeEntryRepository _timeEntryRepo;
+
+    public TimeEntryService(ITimeEntryRepository timeEntryRepo)
+    {
+        _timeEntryRepo = timeEntryRepo;
+    }
+
     public List<TimeEntryResponse> CreateTimeEntry(TimeEntryCreateRequest timeEntryRequest)
     {
         throw new NotImplementedException();
