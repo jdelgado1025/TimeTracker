d0694d606d19b2d1a7280cd9beece218c326389e
Jose Delgado <jdelgado1025@gmail.com>
2025-02-12 21:04:29 -0800

    Registered Service

--- TimeTracker.API/Program.cs
+++ TimeTracker.API/Program.cs
@@ -1,4 +1,5 @@
 using TimeTracker.API.Repositories.Time_Entry;
+using TimeTracker.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
+builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 
 var app = builder.Build();
 
