2dfbc42bf7128622212530fb1fbd6ca064888b5b
Jose Delgado <jdelgado1025@gmail.com>
2025-02-17 17:03:31 -0800

    Added Delete Entry functionality

--- TimeTracker.API/Repositories/Time Entry/TimeEntryRepository.cs
+++ TimeTracker.API/Repositories/Time Entry/TimeEntryRepository.cs
@@ -20,6 +20,17 @@ public class TimeEntryRepository : ITimeEntryRepository
         return _timeEntries;
     }
 
+    public List<TimeEntry>? DeleteTimeEntry(int id, TimeEntry timeEntry)
+    {
+        var entry = _timeEntries.Find(x => x.Id == id);
+
+        if (entry == null)
+            return null;
+
+        _timeEntries.Remove(entry);
+        return _timeEntries;
+    }
+
     public List<TimeEntry> GetAllTimeEntries()
     {
         return _timeEntries;
