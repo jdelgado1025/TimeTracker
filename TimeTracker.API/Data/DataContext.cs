68e630a8fc5d9e2cb9164a9f06f9eb4c4e03c1a0
Jose Delgado <jdelgado1025@gmail.com>
2025-02-22 19:36:13 -0800

    Added & implemented DBContext

---
+++ TimeTracker.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeTracker.API.Data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options): base(options)
+    {
+        
+    }
+
+    public DbSet<TimeEntry> TimeEntries { get; set; }
+}
