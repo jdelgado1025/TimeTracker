21ac6090956c130516b7d0b81974eda9a9ff1123
Jose Delgado <jdelgado1025@gmail.com>
2025-02-22 20:50:35 -0800

    Updated Global Usings for Data

--- TimeTracker.API/GlobalUsings.cs
+++ TimeTracker.API/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using TimeTracker.Domain.Entities;
 global using TimeTracker.API.Repositories.Time_Entry;
 global using TimeTracker.API.Services;
 global using TimeTracker.Domain.Models.TimeEntry;
+global using TimeTracker.API.Data;
