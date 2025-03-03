0e1ba366e5d482b786c16dab9b7cbc42d7b055e1
Jose Delgado <jdelgado1025@gmail.com>
2025-03-02 18:09:55 -0800

    Added custom Not Found exception class

---
+++ TimeTracker.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace TimeTracker.Domain.Exceptions;
+public class EntityNotFoundException: Exception
+{
+    public EntityNotFoundException() : base() { }
+    public EntityNotFoundException(string message) : base(message) { }
+    public EntityNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+}
