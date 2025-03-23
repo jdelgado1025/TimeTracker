31ae89c4a0174ca8ebffc396a8607906bec3f1ee
Jose Delgado <jdelgado1025@gmail.com>
2025-03-22 18:38:18 -0700

    Added Base Class for Entities

---
+++ TimeTracker.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace TimeTracker.Domain.Entities;
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public DateTime DateUpdated { get; set; }
+}
