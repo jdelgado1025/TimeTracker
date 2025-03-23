f248d97ea535e3b358eef660e0e329c73c9689c8
Jose Delgado <jdelgado1025@gmail.com>
2025-03-22 18:52:23 -0700

    Updated DateUpdated Property to be optional

--- TimeTracker.Domain/Entities/BaseEntity.cs
+++ TimeTracker.Domain/Entities/BaseEntity.cs
@@ -3,5 +3,5 @@ public class BaseEntity
 {
     public int Id { get; set; }
     public DateTime DateCreated { get; set; } = DateTime.Now;
-    public DateTime DateUpdated { get; set; }
+    public DateTime? DateUpdated { get; set; }
 }
