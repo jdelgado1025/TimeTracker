1769f19e48e4cd8192755e1af4cd6aff5797e3c9
Jose Delgado <jdelgado1025@gmail.com>
2025-03-22 18:11:50 -0700

    Removed unnecessary usings

--- TimeTracker.Domain/Entities/TimeEntry.cs
+++ TimeTracker.Domain/Entities/TimeEntry.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TimeTracker.Domain.Entities
+﻿namespace TimeTracker.Domain.Entities
 {
     public class TimeEntry
     {
