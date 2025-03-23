15fca6bc59f2aaadae8904cd281b2b3b1f510a90
Jose Delgado <jdelgado1025@gmail.com>
2025-03-22 18:59:22 -0700

    Project Id should match Project for migration relationships - Optional

--- TimeTracker.Domain/Entities/TimeEntry.cs
+++ TimeTracker.Domain/Entities/TimeEntry.cs
@@ -2,7 +2,7 @@
 {
     public class TimeEntry : BaseEntity
     {
-        public int ProjectId { get; set; }
+        public int? ProjectId { get; set; }
         public Project? Project { get; set; }
         public DateTime Start { get; set; } = DateTime.Now;
         public DateTime? End { get; set; }
