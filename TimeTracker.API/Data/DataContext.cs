76b292fdf9c0ada9174f41925e550c1f5adb7d5b
Jose Delgado <jdelgado1025@gmail.com>
2025-04-02 22:16:57 -0700

    Removed Auto Include feature

--- TimeTracker.API/Data/DataContext.cs
+++ TimeTracker.API/Data/DataContext.cs
@@ -7,12 +7,6 @@ public class DataContext: DbContext
         
     }
 
-    //Can also AutoInclude nested objects by default, rather than including them individually
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //    modelBuilder.Entity<TimeEntry>().Navigation(c => c.Project).AutoInclude();
-    //}
-
     public DbSet<TimeEntry> TimeEntries { get; set; }
     public DbSet<Project> Projects { get; set; }
     public DbSet<ProjectDetails> ProjectDetails { get; set; }
