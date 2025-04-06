19d451bae5027a748883b10b3b0c765dd1f573db
Jose Delgado <jdelgado1025@gmail.com>
2025-04-05 20:19:16 -0700

    Added Project Repository and implemented GetAllProjects

---
+++ TimeTracker.API/Repositories/Projects/ProjectRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace TimeTracker.API.Repositories.Projects;
+
+public class ProjectRepository : IProjectRepository
+{
+    private readonly DataContext _context;
+
+    public ProjectRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Project>> CreateProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Project>?> DeleteProject(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Project>> GetAllProjects()
+    {
+        return await _context.Projects
+            .Include(p => p.ProjectDetails)
+            .ToListAsync();
+    }
+
+    public Task<Project?> GetProjectById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<TimeEntry>> GetTimeEntriesByProject(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Project>?> UpdateProject(int id, Project project)
+    {
+        throw new NotImplementedException();
+    }
+}
