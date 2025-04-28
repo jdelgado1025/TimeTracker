26b33a744ee0e68831c3a56a8f5bd0df57d67cf5
Jose Delgado <jdelgado1025@gmail.com>
2025-04-27 17:21:21 -0700

    Project Response Object for Projects with Time Entries

---
+++ TimeTracker.Domain/Models/Project/ProjectTimeEntriesResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeTracker.Domain.Models.Project;
+public record struct ProjectTimeEntriesResponse
+(
+    int Id,
+    string Name,
+    string? Description,
+    DateTime? StartDate,
+    DateTime? EndDate,
+    List<ProjectTimeEntriesSubResponse> TimeEntries
+);
