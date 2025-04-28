d0bcd62a430d29ba40d3be45c472596e2a501355
Jose Delgado <jdelgado1025@gmail.com>
2025-04-27 18:41:21 -0700

    Migration scripts

---
+++ TimeTracker.API/Scripts/UserProjectsRealtions.sql
@@ -0,0 +1,65 @@
+﻿BEGIN TRANSACTION;
+DECLARE @var sysname;
+SELECT @var = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[TimeEntries]') AND [c].[name] = N'Project');
+IF @var IS NOT NULL EXEC(N'ALTER TABLE [TimeEntries] DROP CONSTRAINT [' + @var + '];');
+ALTER TABLE [TimeEntries] DROP COLUMN [Project];
+
+ALTER TABLE [TimeEntries] ADD [ProjectId] int NULL;
+
+CREATE TABLE [Projects] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [DateCreated] datetime2 NOT NULL,
+    [DateUpdated] datetime2 NULL,
+    [IsDeleted] bit NOT NULL,
+    [DateDeleted] datetime2 NULL,
+    CONSTRAINT [PK_Projects] PRIMARY KEY ([Id])
+);
+
+CREATE INDEX [IX_TimeEntries_ProjectId] ON [TimeEntries] ([ProjectId]);
+
+ALTER TABLE [TimeEntries] ADD CONSTRAINT [FK_TimeEntries_Projects_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [Projects] ([Id]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250323015933_Projects', N'9.0.2');
+
+CREATE TABLE [ProjectDetails] (
+    [Id] int NOT NULL IDENTITY,
+    [Description] nvarchar(max) NULL,
+    [StartDate] datetime2 NULL,
+    [EndDate] datetime2 NULL,
+    [ProjectId] int NOT NULL,
+    CONSTRAINT [PK_ProjectDetails] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ProjectDetails_Projects_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [Projects] ([Id]) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX [IX_ProjectDetails_ProjectId] ON [ProjectDetails] ([ProjectId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250323023348_ProjectDetails', N'9.0.2');
+
+CREATE TABLE [Users] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [ProjectUser] (
+    [ProjectsId] int NOT NULL,
+    [UsersId] int NOT NULL,
+    CONSTRAINT [PK_ProjectUser] PRIMARY KEY ([ProjectsId], [UsersId]),
+    CONSTRAINT [FK_ProjectUser_Projects_ProjectsId] FOREIGN KEY ([ProjectsId]) REFERENCES [Projects] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ProjectUser_Users_UsersId] FOREIGN KEY ([UsersId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_ProjectUser_UsersId] ON [ProjectUser] ([UsersId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250323215917_UserProjectRelations', N'9.0.2');
+
+COMMIT;
+GO
+
