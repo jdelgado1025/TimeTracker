add077a071a920f6eb773fb64110ef327eabe844
Jose Delgado <jdelgado1025@gmail.com>
2025-04-27 18:17:16 -0700

    DTO for Time Entries by Project

---
+++ TimeTracker.Domain/Models/TimeEntry/TimeEntryByProjectResponse.cs
@@ -0,0 +1,10 @@
+﻿using TimeTracker.Domain.Models.Project;
+
+namespace TimeTracker.Domain.Models.TimeEntry;
+
+public record struct TimeEntryByProjectResponse
+(
+    int Id,
+    DateTime Start,
+    DateTime? End
+);
