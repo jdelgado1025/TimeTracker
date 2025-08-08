635ce521f734eb696961b5a45d8962341369f6a1
Jose Delgado <jdelgado1025@gmail.com>
2025-08-07 21:53:14 -0700

    Added a service to retrieve the Time Entries

---
+++ TimeTracker.Client/Services/TimeEntries/ITimeEntryService.cs
@@ -0,0 +1,12 @@
+﻿using TimeTracker.Domain.Models.TimeEntry;
+
+namespace TimeTracker.Client.Services.TimeEntries;
+
+public interface ITimeEntryService
+{
+    //Event to subscribe to for displaying Time Entries
+    event Action? OnChange;
+    public List<TimeEntryResponse> TimeEntries { get; set; }
+
+    Task GetTimeEntriesByProject(int projectId);
+}
---
+++ TimeTracker.Client/Services/TimeEntries/TimeEntryService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+using TimeTracker.Domain.Models.TimeEntry;
+
+namespace TimeTracker.Client.Services.TimeEntries;
+
+public class TimeEntryService : ITimeEntryService
+{
+    public List<TimeEntryResponse> TimeEntries { get; set; } = new();
+
+    public event Action? OnChange;
+    private readonly HttpClient _httpClient;
+
+    public TimeEntryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task GetTimeEntriesByProject(int projectId)
+    {
+        var timeEntries = (projectId <= 0) ? 
+            await _httpClient.GetFromJsonAsync<List<TimeEntryResponse>>("api/timeentry") : await _httpClient.GetFromJsonAsync<List<TimeEntryResponse>>($"api/timeentry/project/{projectId}");
+
+        if(timeEntries is not null)
+        {
+            TimeEntries = timeEntries;
+            OnChange?.Invoke();
+        }
+
+
+    }
+}
